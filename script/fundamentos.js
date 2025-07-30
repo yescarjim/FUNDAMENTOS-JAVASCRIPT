bcb568d717975a4a7f9195738fd1518f5c14b31d
yescarjim <yesmykarito@gmail.com>
2025-07-29 23:02:03 -0500

    Creacion fundamentos JavaScript

---
+++ script/fundamentos.js
@@ -0,0 +1,51 @@
+// Comentario en linea
+/* Comentario 
+en Bloque*/
+
+//---------VARIABLES----------
+//declaracin de variables
+let nombre = "Carolina"; //Variable que cambia en el tiempo
+const PI = 3.14; //Variable Constante
+
+
+//-------TIPOS DE DATOS-------
+//PRIMITIVOS
+//Numericos
+let numero1 = 10;  //entero
+let numero2 = 5.4; //decimal
+
+//string(texto)
+let texto1 = "a";
+let texto2 = "2";
+let texto3 = "palabras";
+let texto4 = "una frase mas larga, incluso un parrafo";
+
+//buleanos (verdad o falsedad)
+let isVerdad = true; //verdad
+let isFalso = false;//falso
+
+//Nulos o indefinidos
+let indefinido = undefined;
+let nulo = null;
+
+
+//NO PRIMITIVOS
+
+//array (arreglos de datos) -- [corchetes]
+let arrayNombres = ["Samuel","Cristofher","Sebastian","Jorge"]
+let array = ["luisa",l, true, null]; //ES MALA PESIMA PRACTICA
+let arrayNumeros= [1 ,3 ,6 ,1000];
+
+//objetos(representacion real -) {llaves}
+// "clave" : "valor" 
+let persona ={
+    nombre : "Yesmy",
+    apellido : "Jiménez",
+    edad : 25,
+    }
+
+    //funciones - bloques de código - acciones
+    function sumar(){
+        return number1 + number2;
+
+    }
